271d9e3ee639ad859521264388cceec71b82aec2
ekarpp <eetu@karppinen.xyz>
2024-02-04 22:31:49 +0200

    add audiokeys

--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 
 static const struct bind binds[] = {
 	/* mod,         key,    cmd */
-	{ ControlMask,  XK_t,   "xterm" },
+	{ 0,		XF86XK_AudioLowerVolume,	"amixer sset Master 5%- -q" },
+	{ 0,		XF86XK_AudioMute,		"amixer sset Master toggle -q" },
+	{ 0,		XF86XK_AudioRaiseVolume,	"amixer sset Master 5%+ -q" }
 };
 
