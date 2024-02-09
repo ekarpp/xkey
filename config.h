fb4e87fb49efa7bc45d344d6793a7c440d747a56
ekarpp <eetu@karppinen.xyz>
2024-02-09 21:31:42 +0200

    add mic toggle

--- config.h
+++ config.h
@@ -6,5 +6,6 @@ static const struct bind binds[] = {
 	{ 0,		XF86XK_AudioMute,		"amixer sset Master toggle -q" },
 	{ 0,		XF86XK_AudioRaiseVolume,	"amixer sset Master 5%+ -q" },
         { 0,		XF86XK_MonBrightnessDown,	"sudo brightness down" },
-	{ 0,		XF86XK_MonBrightnessUp,		"sudo brightness up" }
+	{ 0,		XF86XK_MonBrightnessUp,		"sudo brightness up" },
+	{ 0,		XF86XK_AudioMicMute,		"amixer sset Capture toggle -q" }
 };
