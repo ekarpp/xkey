723ec5c4003588c99b2217ee0673b696f4051e07
ekarpp <eetu@karppinen.xyz>
2024-02-10 10:52:20 +0200

    add screenshot and send signals to dwmblocks

--- config.h
+++ config.h
@@ -2,10 +2,11 @@
 
 static const struct bind binds[] = {
 	/* mod,         key,    cmd */
-	{ 0,		XF86XK_AudioLowerVolume,	"amixer sset Master 5%- -q" },
-	{ 0,		XF86XK_AudioMute,		"amixer sset Master toggle -q" },
-	{ 0,		XF86XK_AudioRaiseVolume,	"amixer sset Master 5%+ -q" },
-        { 0,		XF86XK_MonBrightnessDown,	"sudo brightness down" },
-	{ 0,		XF86XK_MonBrightnessUp,		"sudo brightness up" },
-	{ 0,		XF86XK_AudioMicMute,		"amixer sset Capture toggle -q" }
+	{ 0,		XF86XK_AudioLowerVolume,	"amixer sset Master 5%- -q && pkill -RTMIN+2 dwmblocks" },
+	{ 0,		XF86XK_AudioMute,		"amixer sset Master toggle -q && pkill -RTMIN+2 dwmblocks" },
+	{ 0,		XF86XK_AudioRaiseVolume,	"amixer sset Master 5%+ -q && pkill -RTMIN+2 dwmblocks" },
+        { 0,		XF86XK_MonBrightnessDown,	"sudo brightness down && pkill -RTMIN+1 dwmblocks" },
+	{ 0,		XF86XK_MonBrightnessUp,		"sudo brightness up && pkill -RTMIN+1 dwmblocks" },
+	{ 0,		XF86XK_AudioMicMute,		"amixer sset Capture toggle -q && pkill -RTMIN+3 dwmblocks" },
+	{ 0,		0xff61,				"scrot -s" }
 };
